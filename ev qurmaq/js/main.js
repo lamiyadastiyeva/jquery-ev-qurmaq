e20b788cfb86b8015fb9321cbfb74069c7b6c0aa
lamiyadastiyeva <lamiyadastiyeva@gmail.com>
2023-04-07 22:46:16 +0400

    jquery ev
    jquery vasitesile ev qurmaq

---
+++ ev qurmaq/js/main.js
@@ -0,0 +1,25 @@
+$(function(){
+
+    $("#hasar-btn").click(function() {
+        $("#hasar").toggleClass('has')
+    
+    })
+    
+    $("#light").click(function(){
+        $(".window").toggleClass("win")
+
+    })
+    $("#hours").click(function(){
+        $("#moon").toggleClass("moon-go")
+        $("#sun").toggleClass("sun-go")
+        $(".container").toggleClass("white")
+        $('.colorum').toggleClass("whit")
+        
+    })
+    
+
+
+
+
+})
+
